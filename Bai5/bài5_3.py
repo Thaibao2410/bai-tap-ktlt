a2631979c60f9429c0d044896db9222cdb24804c
Thaibao2410 <bao9athcstt@gmail.com>
2025-05-17 20:14:03 +0700

    Add files via upload

---
+++ Bai5/bài5_3.py
@@ -0,0 +1,5 @@
+import numpy as np
+x = np.arange (12, 38)
+print(x)
+print("nguyễn thái bảo")   
+print("MSSV: 235752020710028") 
---
+++ Bai5/bài5_4.py
@@ -0,0 +1,8 @@
+import numpy as np
+mang = np.arange(12, 39)
+print("Mảng ban đầu:",mang)
+
+mang_dao_nguoc = np.flip(mang)
+print("Mảng đảo ngược:", mang_dao_nguoc)
+print("nguyễn thái bảo")   
+print("MSSV: 235752020710028") 
